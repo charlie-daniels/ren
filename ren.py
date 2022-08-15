cf4f0513964e54aaa5be6f6d87f7ff742bb30671
charlie-daniels <charlied030401@gmail.com>
2022-08-15 14:30:38 +0100

    Add SortingOptions functionality
    added reverse and add date to dataclass
    
    resolves #1

--- ren.py
+++ ren.py
@@ -12,8 +12,10 @@ class SortingMethod(Enum):
 
 @dataclass
 class SortingOptions:
+    reverse: bool = False
+    add_date: bool = False
     file_ext: str = '*.*'
-    
+
 def print_welcome_screen() -> None:
     print('Welcome to ren!')
 
@@ -40,11 +42,11 @@ def handle_sort(target_directory: str, method: SortingMethod, options: SortingOp
     file_list = glob.glob(target_directory + options.file_ext)
 
     if method == SortingMethod.ALPHABETICAL:
-        return sorted(file_list)
+        return sorted(file_list, reverse=options.reverse)
     elif method == SortingMethod.CHRONOLOGICAL:     
-        return sorted(file_list,key=getmtime)
+        return sorted(file_list, key=getmtime, reverse=options.reverse)
     elif method == SortingMethod.FILE_SIZE:
-        return sorted(file_list,key=getsize) # Returns largest last
+        return sorted(file_list, key=getsize, reverse=options.reverse) # Returns largest last
 
 def rename_file_list(target_directory: str, sorted_files: list[str], bulk_name: str, add_date: bool) -> list[str]:
     '''Applies the set naming convention to a new list of filepaths.'''
@@ -52,12 +54,13 @@ def rename_file_list(target_directory: str, sorted_files: list[str], bulk_name: 
     for file_index, current_file in enumerate(sorted_files):
         _path, file_ext = splitext(current_file)
         # Add zero placeholders of length of list
-        file_index_zfill = str(file_index).zfill(len(str(len(sorted_files))))       
+        file_index_zfill = str(file_index).zfill(len(str(len(sorted_files))))     
+
         if add_date:
             current_date = datetime.today().strftime('%Y-%m-%d')
-            new_file_name = f'{target_directory}{current_date}-{file_index_zfill}-{bulk_name}{file_ext}'
+            new_file_name = f'{target_directory}{current_date}_{file_index_zfill}_{bulk_name}{file_ext}'
         else:
-            new_file_name = f'{target_directory}{file_index_zfill}{bulk_name}{file_ext}'
+            new_file_name = f'{target_directory}{file_index_zfill}_{bulk_name}{file_ext}'
         renamed_files.append(new_file_name)
     return renamed_files
 
@@ -69,11 +72,16 @@ def apply_rename(original_files: list[str], renamed_files: list[str]) -> None:
     return 
 
 def get_sorting_options() -> SortingOptions:
-    print('\nFor any file type, use .*')
-    ext = input('Enter file type: ')
-    ext = f'*{ext}'
+    print('\nEnter 1 for true, 0 for false')
 
-    options = SortingOptions(file_ext=ext)
+    reverse = bool(int(input('\nReverse order: ')))
+    add_date = bool(int(input('\nUse date: ')))
+
+    print('\nFor any file type, use .*')
+    file_ext = input('Enter file type: ')
+    file_ext = f'*{file_ext}'
+
+    options = SortingOptions(reverse=reverse, add_date=add_date, file_ext=file_ext)
 
     return options
 
@@ -82,12 +90,11 @@ def main():
     
     target_directory = get_target_directory()
     selected_sort = get_sorting_method()
-    add_date = bool(input('Use date (True / False): '))
     sorting_options = get_sorting_options()
     sorted_files = handle_sort(target_directory, selected_sort, sorting_options)
 
     bulk_name = input('\nEnter name for all files: ')
-    renamed_files = rename_file_list(target_directory, sorted_files, bulk_name, add_date)
+    renamed_files = rename_file_list(target_directory, sorted_files, bulk_name, sorting_options.add_date)
     apply_rename(sorted_files, renamed_files)
     print('Files renamed.')
 
